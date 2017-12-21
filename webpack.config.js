3e5772d92233c6580006e4b9657242f0838bd7af
Joe Gilsenan <joseph.gilsenan@mnetics.co.uk>
2017-12-21 17:50:33 +0000

    Latest

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,33 @@ module.exports = {
     ],
     output: {
         filename:"bundle.js"
+    },
+    module:{
+        preLoaders:[
+            {
+                test: /\.js$/,
+                exclude:'node_modules',
+                loader:'jshint-loader'
+
+            }
+        ],
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude:'node_modules',
+                loader:'jshint-loader'
+
+            },
+            {
+                test: /\.es6$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    },
+
+    resolve:{
+        extensions: ['','.js','.es6']
     }
+
 }
